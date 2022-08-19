1a9c8e6e971de46f069dc6e033e0885e1a042cd3
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-08-19 12:34:58 +0530

    added logic to js file

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const textareaInput = document.querySelector ("#textarea-input");
 const translateButton = document.querySelector ("#translate-button");
 const output = document.querySelector ("#output");
 
-var serverURL = ""
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
-translateButton.addEventListener ("click", () => {
-    console.log (textareaInput.value);
-})
+translateButton.addEventListener ("click", clickHandler);
+
+function getTranslationURL (text) {
+    return serverURL + "?" + "text="+text;
+}
+
+function errorHandler (error) {
+    console.log ("Error occuered ",error);
+}
+
+function clickHandler () {
+    fetch (getTranslationURL (textareaInput.value))
+    .then (Response => Response.json ())
+    .then (json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText;
+    })
+    .catch (errorHandler);
+}
